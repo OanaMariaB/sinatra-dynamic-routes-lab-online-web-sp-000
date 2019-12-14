34a6c3e394ed5dae93a3625428ec8a340e82dda7
Oana Berentan <oana.berentan@yahoo.com>
2019-12-14 20:12:48 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -3,4 +3,17 @@ require_relative 'config/environment'
 class App < Sinatra::Base
   # Write your code here!
 
+    get '/reversename/:name' do
+      @reverse_name = params[:name].reverse
+      @reverse_name
+    end
+
+    get '/square/:number' do
+      @square_number = params[:number].to_i ** 2
+      @square_number.to_s
+    end
+
+    get '/say/:number/:phrase' do
+
+    end
 end
